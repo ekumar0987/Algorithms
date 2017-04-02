b81e3327d08543758de138cc08b7fb2c50e22f37
ekumar0987 <hk.eshitha@gmail.com>
2017-04-02 10:57:15 -0500

    4/2

--- Python/CC/033b. setOfStacks.py
+++ Python/CC/033b. setOfStacks.py
@@ -1,3 +1,110 @@
 """
 Set of stacks implementation - pop from a specific stack and move all elements to left
 """
+
+class Stack:
+	def __init__(self):
+		self.items = []
+		self.max_size = 5
+
+	def isEmpty(self):
+		return self.items == []
+
+	def push(self, item):
+		self.items.append(item)
+
+	def pop(self):
+		return self.items.pop()
+
+	def peek(self):
+		return self.items[len(self.items)-1]
+
+	def size(self):
+		return len(self.items)
+	
+	# extra methods
+	def isFull(self):
+		return len(self.items) == self.max_size
+		
+	def printStack(self):
+		print "Printing stack..."
+		for i in self.items:
+			print i
+			
+# customized push and pop operations	
+class SetOfStacks:
+	def __init__(self):
+		self.set_of_stacks = []           
+		self.current_stack_no = 0
+		
+	def isEmpty(self):
+		return self.set_of_stacks == []
+		 
+	def push(self, item):
+		
+		# set of stacks is empty
+		if self.isEmpty():
+			# create new stack, push item and add stack to list of stacks
+			current_stack = Stack()
+			self.current_stack_no = 0
+			current_stack.push(item)
+			self.set_of_stacks.append(current_stack)
+		else:
+			# get the current stack and push item
+			current_stack = self.set_of_stacks[self.current_stack_no]
+			if current_stack.isFull():
+				new_stack = Stack()
+				new_stack.push(item)
+				self.set_of_stacks.append(new_stack)
+				self.current_stack_no = self.current_stack_no + 1
+			else:
+				current_stack.push(item)
+
+	def pop(self, stackNo):
+		
+		if self.isEmpty() or stackNo > current_stack_no:
+			return 
+		
+		# get stack indicated by stack no and and pop item
+		stack = self.set_of_stacks[stackNo]
+		result = stack.pop()
+		
+		# If the stack has no more elements, delete it from the set of stacks
+		if stack.isEmpty():
+			del self.set_of_stacks[self.stackNo]
+			self.current_stack_no = self.current_stack_no - 1
+		
+		##############################Extra###################################
+		# Stack not empty, move element from next stack to this stack and so on to fill the hole
+		# A tmp stack is used as a buffer 
+		else:
+			#loop over set of stacks starting from stack no
+			start_stack = stackNo
+			for i in range(start_stack, len(self.set_of_stacks)):
+				
+				# copy over not need for the last stack
+				if start_stack == len(self.set_of_stacks) - 1:
+					break
+				
+				tmp_stack = Stack()
+				
+				# get next stack and copy all its elements to tmp
+				next_stack = self.set_of_stacks[start_stack + 1]
+				
+				while not next_stack.isEmpty():
+					tmp_stack.push(next_stack.pop())
+				
+				# pop 1 element from tmp into start_stack to fill hole
+				self.set_of_stacks[start_stack].push(tmp_stack.pop())
+				
+				while not tmp_stack.isEmpty():
+					next_stack.push(tmp_Stack.pop())
+				
+				
+		return result
+			
+	def printSetOfStacks(self):
+		
+		for s in self.set_of_stacks:
+			s.printStack()
+			print "Next stack..."
